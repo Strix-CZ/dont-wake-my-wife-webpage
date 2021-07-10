f5a4f69b131a9dd9f418fcdd6a907337fb644776
Jan Simonek <jan.simonek@gmail.com>
2021-07-10 15:07:03 +0200

    Fixing Access-Control-Allow-Origin from all origins

--- server/src/main/java/online/temer/alarm/server/Server.java
+++ server/src/main/java/online/temer/alarm/server/Server.java
@@ -27,6 +27,29 @@ public class Server
 		Spark.get("/alarm", getAlarmHandler);
 		Spark.post("/alarm", setAlarmHandler);
 
+
+		Spark.options("/*",
+				(request, response) -> {
+
+					String accessControlRequestHeaders = request
+							.headers("Access-Control-Request-Headers");
+					if (accessControlRequestHeaders != null) {
+						response.header("Access-Control-Allow-Headers",
+								accessControlRequestHeaders);
+					}
+
+					String accessControlRequestMethod = request
+							.headers("Access-Control-Request-Method");
+					if (accessControlRequestMethod != null) {
+						response.header("Access-Control-Allow-Methods",
+								accessControlRequestMethod);
+					}
+
+					return "OK";
+				});
+
+		Spark.before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
 		Spark.awaitInitialization();
 	}
 
